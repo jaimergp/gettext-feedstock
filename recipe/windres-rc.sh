6a319f4a06371c17134129db1b4761164a665913
jaimergp <jaimergp@users.noreply.github.com>
2026-08-27 11:28:00 +0200

    try with wrapper found elsewhere

--- recipe/windres-rc.sh
+++ recipe/windres-rc.sh
@@ -1,55 +1,55 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/sh
+# Taken and adapted from:
+# https://github.com/jgaskins/crystal/blob/614aea7ec62ed426602b2a86fbbda789dc969a47/etc/win-ci/windres-rc.sh
 
-flags=()
-input_file=""
+# Minimal wrapper for Microsoft rc.exe
 
-next_is_out=0
+# The build will call something like:
+#
+#   libtool: compile:  $windres
+#     -DPACKAGE_VERSION_STRING=\"0.22.5\" 
+#     -DPACKAGE_VERSION_MAJOR=0 
+#     -DPACKAGE_VERSION_MINOR=22 
+#     -DPACKAGE_VERSION_SUBMINOR=5 
+#     -i ./libintl.rc 
+#     --output-format=coff  
+#     -o .libs/libintl.res.obj
+#
+# the corresponding rc.exe invocation is:
+#
+#     rc.exe /NOLOGO 
+#       -DPACKAGE_VERSION_STRING=\"0.22.5\" 
+#       -DPACKAGE_VERSION_MAJOR=0 
+#       -DPACKAGE_VERSION_MINOR=22 
+#       -DPACKAGE_VERSION_SUBMINOR=5 
+#       /FO .libs/libintl.res.obj libintl.rc
 
-for arg in "$@"; do
-    if [[ $next_is_out -eq 1 ]]; then
-        flags+=("/fo" "$arg")
-        next_is_out=0
-        continue
-    fi
+RC=$1
+input=
+output=
+defines=""
 
-    case "$arg" in
-        -o)
-            next_is_out=1
-            ;;
-        -o*)
-            out_file="${arg#-o}"
-            flags+=("/fo" "${out_file}")
-            ;;
-        -i)
-            # Ignore windres input flag
-            ;;
-        -D*)
-            macro="${arg#-D}"
-            # Strip literal quote characters that break rc.exe /d flags
-            macro="${macro//\"/}"
-            flags+=("/d" "${macro}")
-            ;;
-        -I*)
-            include_dir="${arg#-I}"
-            flags+=("/i" "${include_dir}")
-            ;;
-        --output-format=*|-O*)
-            # Drop GNU windres format flags
-            ;;
-        *.rc)
-            input_file="$arg"
-            ;;
-        *)
-            flags+=("$arg")
-            ;;
-    esac
+while [ $# -gt 0 ]; do
+  case $1 in
+    -D* | -d*)
+      defines="$1 ${defines}"
+      shift
+      ;;
+    -i)
+      shift
+      input=$1
+      shift
+      ;;
+    -o)
+      shift
+      output=$1
+      shift
+      ;;
+    *)
+      # ignore --output-format=coff as it is the default already
+      shift
+      ;;
+  esac
 done
 
-set -x
-# Ensure input file is passed strictly at the very end
-if [[ -n "$input_file" ]]; then
-    exec rc.exe "${flags[@]}" "$input_file"
-else
-    exec rc.exe "${flags[@]}"
-fi
+exec rc.exe -NOLOGO $defines -FO "$output" "$input"
